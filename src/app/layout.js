e9c5962e5cee5d191eceb4a5b201318764bdcff8
Pablo Hinojosa <pablo.hinojosa@ort.edu.ar>
2026-09-09 01:06:15 +0000

    fix: en layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,10 +22,11 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..2" />
+      </head>
       <body className="min-h-full flex flex-col">
-        <h3>Home | Clientes</h3>
         {children}
-        <h3>Footer</h3>
       </body>
     </html>
   );
